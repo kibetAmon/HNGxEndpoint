ad72fcf395a757606873d53493e45b68e40c2aec
KibetAmon <kibetngetich805@gmail.com>
2023-09-08 23:00:00 +0300

    Created the endpoint function

--- endpoint/myendpoint/views.py
+++ endpoint/myendpoint/views.py
@@ -1,15 +1,19 @@
 from django.http import JsonResponse
+from datetime import datetime, timezone, date
 
+import calendar
 
 # Create your views here.
 def endpointview(request):
+    today = date.today()
     data = {
         'slack_name': 'Amon Kibet',
-        'current_date':'',
-        'utc_time':'',
-        'track':'',
-        'github_file_url':'',
+        'current_day':calendar.day_name[today.weekday()],
+        'utc_time': datetime.now(timezone.utc),
+        'track':'Backend',
+        'github_file_url':'https://github.com/kibetAmon/HNGxEndpoint/blob/master/endpoint/myendpoint/views.py',
         'github_file_repo':'https://github.com/kibetAmon/HNGxEndpoint',
-
-
+        'status_code': 200
     }
+    response = JsonResponse(data, status=200)
+    return response
