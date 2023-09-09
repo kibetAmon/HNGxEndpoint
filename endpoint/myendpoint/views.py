3b1230ec7ca9633634e005cec0e0bce1314fd673
KibetAmon <kibetngetich805@gmail.com>
2023-09-09 03:39:34 +0300

    Edit slack name

--- endpoint/myendpoint/views.py
+++ endpoint/myendpoint/views.py
@@ -3,16 +3,17 @@ from datetime import datetime, timezone, date
 
 import calendar
 
+
 # Create your views here.
 def endpointview(request):
     today = date.today()
     data = {
-        'slack_name': 'amon_Kibet',
-        'current_day':calendar.day_name[today.weekday()],
+        'slack_name': 'Amon Kibet',
+        'current_day': calendar.day_name[today.weekday()],
         'utc_time': datetime.now(timezone.utc),
-        'track':'backend',
-        'github_file_url':'https://github.com/kibetAmon/HNGxEndpoint/blob/master/endpoint/myendpoint/views.py',
-        'github_file_repo':'https://github.com/kibetAmon/HNGxEndpoint',
+        'track': 'backend',
+        'github_file_url': 'https://github.com/kibetAmon/HNGxEndpoint/blob/master/endpoint/myendpoint/views.py',
+        'github_file_repo': 'https://github.com/kibetAmon/HNGxEndpoint',
         'status_code': 200
     }
     response = JsonResponse(data, status=200)
