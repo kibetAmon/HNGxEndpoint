d05387ce9cd88e68f613adc3cd8c26f735185ff5
KibetAmon <kibetngetich805@gmail.com>
2023-09-09 03:44:37 +0300

    Edited the url file

--- endpoint/endpoint/urls.py
+++ endpoint/endpoint/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("endpoint/", include("myendpoint.urls")),
+    path("/", include("myendpoint.urls")),
 ]
